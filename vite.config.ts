5cbfe447a11c9798b88a62b373b6d6e635358573
Ying Quan Tan <ying.quan.tan@gmail.com>
2023-10-23 13:18:20 +0800

    adjust base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import dsv from '@rollup/plugin-dsv';
 export default defineConfig({
   assetsInclude: ['**/*.geojson'],
   plugins:[dsv()],
+  base: '/dsny-baskets', // github page is served at brightredchilli.github.io/dsny-baskets
   build: {
     outDir: 'docs/'
   }
