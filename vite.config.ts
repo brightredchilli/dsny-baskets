c1157aa26fe32f3ddfb4740bdf6b1d5fadc2545f
Ying Quan Tan <751191+brightredchilli@users.noreply.github.com>
2025-07-25 16:15:39 -0400

    updated to use the vite tailwindcss plugin, eliminating the need for postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import dsv from '@rollup/plugin-dsv';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   assetsInclude: ['**/*.geojson'],
-  plugins: [dsv()],
+  plugins: [dsv(), tailwindcss()],
   base: '/dsny-baskets', // github page is served at brightredchilli.github.io/dsny-baskets
   build: {
     outDir: 'dist/'
